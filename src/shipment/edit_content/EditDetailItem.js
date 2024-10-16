3752946ad2351fa477735268a2cfad2a1a9da06b
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2024-10-16 12:35:09 -0600

    feat: se implementaron los eventos para editar la cantidad de la tabla

--- src/shipment/edit_content/EditDetailItem.js
+++ src/shipment/edit_content/EditDetailItem.js
@@ -1,8 +1,10 @@
 export class HandleEventManagerEditIDetailItem {
 	constructor(Shipment) {
-		this.Shipment = Shipment;
+		this.ShipmentDetails = Shipment?.Details?.[0]?.ShipmentDetail ?? [];
 
-		this.currentRow = null; // -> Value defined en [handleEventClickInTable]
+		this.currentRow = null;
+		this.currentLineNumber = null;
+		this.currentLabelQty = null;
 	}
 
 	handleEventClickInTable(target) {
@@ -47,7 +49,7 @@ export class HandleEventManagerEditIDetailItem {
 		console.log(dataset);
 
 		const actions = {
-			editRow: () => this.editRow(lineNumber),
+			editRow: () => this.editRow(),
 			deleteRow: () => console.log("ELIMINAR FILA"),
 		};
 
@@ -58,21 +60,26 @@ export class HandleEventManagerEditIDetailItem {
 		}
 
 		action();
+		this.currentLineNumber = lineNumber;
 	}
 
 	// Método principal para editar los capoos de la tabla
-	editRow(lineNumber) {
-		// Aquí puedes implementar la lógica para editar la fila
-
+	editRow() {
 		// const tdItem = this.currentRow.querySelector(".td-item");
 		const tdQuantity = this.currentRow.querySelector(".td-quantity");
 
 		if (tdQuantity) {
-			this.editCell(tdQuantity, lineNumber);
+			this.editCell(tdQuantity);
 		}
 	}
 
-	editCell(td, lineNumber) {
+	/**
+	 * Método para editar un campo de la tabla
+	 * Se agregan eventos de  teclado para que el usuario pueda editar el campo
+	 *@param {TD} td elemento actual a  editar
+
+	 */
+	editCell(td) {
 		const currentLabel = td.querySelector("label");
 
 		if (!currentLabel) {
@@ -80,6 +87,8 @@ export class HandleEventManagerEditIDetailItem {
 				"Mostrar error al usuario\nHa ocurrido un problema al editar el contenido"
 			);
 		}
+
+		this.currentLabelQty = currentLabel;
 
 		const currentValue = currentLabel.textContent.trim();
 		const input = td.querySelector("input");
@@ -91,38 +100,10 @@ export class HandleEventManagerEditIDetailItem {
 		input.value = currentValue;
 		input.dataset.currentValue = currentValue;
 
-		this.currentRow.classList.add("editing");
+		td.classList.add("editing");
 
 		this.insertEvent(input);
 		input.focus();
-
-		const editConten = (newValue) => {
-			const Shipment =
-				this.ShipmentOriginal.WMWROOT?.WMWDATA?.[0]?.Shipments?.[0]
-					?.Shipment?.[0];
-
-			const ShipmentDetails = Shipment?.Details?.[0]?.ShipmentDetail ?? [];
-
-			// const objectToModify = ShipmentDetails?.[numericIndex];
-
-			if (ShipmentDetails?.[numericIndex]) {
-				if (ShipmentDetails?.[numericIndex]?.RequestedQty?.[0]) {
-					ShipmentDetails[numericIndex].RequestedQty[0] = newValue;
-				}
-
-				if (ShipmentDetails?.[numericIndex]?.TotalQuantity?.[0]) {
-					ShipmentDetails[numericIndex].TotalQuantity[0] = newValue;
-				}
-
-				if (ShipmentDetails?.[numericIndex]?.SKU?.[0]?.Quantity?.[0]) {
-					ShipmentDetails[numericIndex].SKU[0].Quantity[0] = newValue;
-				}
-			}
-
-			currentLabel.innerHTML = newValue;
-
-			closeInput();
-		};
 	}
 
 	deleteCell(target, index) {
@@ -154,69 +135,100 @@ export class HandleEventManagerEditIDetailItem {
 		tr.remove();
 	}
 
-	/*
-	 REVISAR METODOS
-	*/
+	/**
+	 * Buscar el índice del objeto con el valor en ErpOrderLineNum que coincide
+	 * @param {*} currentLineNumber
+	 * @returns  {number}  índice del objeto
+	 */
+	getIndex(currentLineNumber) {
+		return this.ShipmentDetails.findIndex(
+			(detail) => detail?.ErpOrderLineNum?.[0] === currentLineNumber
+		);
+	}
+
+	handleEditQuantity(newValue, clearEventInput) {
+		const { currentLineNumber, ShipmentDetails } = this;
+		const index = this.getIndex(currentLineNumber);
+
+		// Si se encuentra, actualiza el campo RequestedQty
+		if (index !== -1) {
+			ShipmentDetails[index].RequestedQty = [newValue];
+			ShipmentDetails[index].TotalQuantity = [newValue];
+
+			if (ShipmentDetails[index]?.SKU?.[0]?.Quantity) {
+				ShipmentDetails[index].SKU[0].Quantity = [newValue];
+			}
+		} else {
+			console.warn(
+				"No se encontró un objeto con el ErpOrderLineNum especificado."
+			);
+		}
+
+		console.log("New value:", newValue);
+		console.log({ ShipmentDetails });
+
+		this.currentLabelQty.textContent = newValue;
+		if (typeof clearEventInput === "function") clearEventInput();
+	}
 
 	// Cierra el input y remueve el modo edición
-	closeInput = (input) => {
+	clearEventInput = (input) => {
+		if (!input) return;
+
 		input.value = "";
-		this.currentRow.classList.remove("editing");
+		input.closest("td")?.classList?.remove("editing");
 		input.removeEventListener("keydown", this.handleInputEvents);
 		input.removeEventListener("blur", this.handleInputEvents);
 	};
 
-	// Edita el contenido de Shipment
-	editContent = (newValue) => {
-		const Shipment =
-			this.ShipmentOriginal?.WMWROOT?.WMWDATA?.[0]?.Shipments?.[0]
-				?.Shipment?.[0];
+	/**
+	 * Edita el contenido del Objeto Shipment Details por  medio de un input
+	 * @param {String} updateField Valor del  campo a actualizar por ejemplo : `quantity` o `item`
 
-		if (!Shipment) return this.showUserError("Error en datos de Shipment");
-
-		const fieldMap = {
-			shipmentId: "ShipmentId",
-			erpOrder: "ErpOrder",
-		};
-
-		const fieldKey = fieldMap[this.updateField];
-
-		if (fieldKey) {
-			Shipment[fieldKey] = [newValue];
-		} else {
-			this.showUserError(`No se puede editar el campo: ${this.updateField}`);
+	 * @param {String} newValue Valor obtenido del imput
+	 */
+	editContent = (updateField, newValue, clearEventInput) => {
+		if (!this.ShipmentDetails || !this.ShipmentDetails?.length) {
+			return this.showUserError("Error en datos de Shipment");
 		}
 
-		console.log("New value:", newValue);
-		console.log({ Shipment });
+		const fieldMap = {
+			quantity: () => this.handleEditQuantity(newValue, clearEventInput),
+		};
 
-		this.currentLabel.textContent = newValue;
-		this.closeInput();
+		if (!fieldMap[updateField]) {
+			this.showUserError(`No se puede editar el campo: ${updateField}`);
+			if (typeof clearEventInput === "function") clearEventInput();
+			return;
+		}
+
+		fieldMap[updateField]();
 	};
 
 	// Maneja eventos de blur y keydown
 	handleInputEvents = (event) => {
 		const { target: input } = event;
+		const { currentValue, updateField } = input.dataset;
 
 		if (event.type === "blur" || event.key === "Enter") {
 			const newValue = input.value.trim();
-			const currentValue = input.dataset.currentValue;
 
 			if (!isNaN(Number(newValue)) && Number(newValue) < 0) {
 				return;
 			}
 
 			if (newValue === currentValue) {
-				this.closeInput(input);
+				this.clearEventInput(input);
 				return;
 			}
 
-			// this.editContent(newValue);
-			console.log("EDITAR CONTENIDO en el EVENTO");
+			this.editContent(updateField, newValue, () => {
+				this.clearEventInput(input);
+			});
 		}
 
 		if (event.key === "Escape") {
-			this.closeInput(input);
+			this.clearEventInput(input);
 		}
 	};
 
@@ -238,4 +250,8 @@ export class HandleEventManagerEditIDetailItem {
  * * si existe  un campo con la clase [.editing] en la fila
  * * si existe quitar la clase y retornar
  * * en caso contrario continuar con el flujo de la funcion
+ * 
+ * 
+ * ? Agregar un toggle class al button  de editar
+
  */
