4ffa2523f5e6615018a469fb9b506e238356f68d
Jose Carlos AR <101861240+JoseCarlos00@users.noreply.github.com>
2024-10-17 00:30:29 -0600

    Refacor: [renderer]: se agrego un bloque try-catch a la function handleOpenFile

--- renderer.js
+++ renderer.js
@@ -6,24 +6,36 @@ const xmlContentContainer = document.getElementById("xml-content");
 openFileButton.addEventListener("click", handleOpenFile);
 
 async function handleOpenFile() {
-	const fileContent = await window.fileApi.selectFile();
+	try {
+		const fileContent = await window.fileApi.selectFile();
 
-	console.log({ fileContent });
+		console.log({ fileContent });
 
-	if (!fileContent?.shipment || !fileContent) {
-		console.log("No se pudo obtener el contenido del archivo.");
-		return;
+		if (!fileContent?.shipment || !fileContent) {
+			throw new Error("No se pudo obtener el contenido del archivo.");
+		}
+
+		xmlContentContainer.innerHTML = "";
+
+		const shipment = new ShipmentManager({
+			Shipment: fileContent.shipment,
+			ShipmentOriginal: fileContent.ShipmentOriginal,
+			FileName: fileContent.fileName,
+		});
+
+		shipment.render();
+	} catch (error) {
+		console.error("Detalles del error:", error);
+		showUserError("No se pudo abrir el archivo.");
 	}
+}
 
-	xmlContentContainer.innerHTML = "";
-
-	const shipment = new ShipmentManager({
-		Shipment: fileContent.shipment,
-		ShipmentOriginal: fileContent.ShipmentOriginal,
-		FileName: fileContent.fileName,
-	});
-
-	shipment.render();
+/**
+ * Muestra un mensaje de error al usuario y lo registra en la consola.
+ * @param {string} message - Mensaje de error a mostrar.
+ */
+function showUserError(message) {
+	alert(message);
 }
 
 // Escuchar el evento desde el menú para abrir el archivo
